bd9123e6c1fb31da3c1828c29dfeadaaacbe6868
Michal Kubišta <michal.kubista@nielsen.com>
2018-03-21 10:31:17 +0100

    Transfer.

---
+++ etl.R
@@ -0,0 +1,11 @@
+# 1.) Report the sales, quantity and mean price per subcategory in period
+#     2014-02-01 to 2016-12-31
+# 
+# 2.) Report the customer penetration per subcategory
+#     (which % of customers have bought the subcategory)
+# 
+# 3.) Report the proportion of:
+#                 - consumer
+#                 - corporate
+#                 - home office
+#     customers per subcategory.
